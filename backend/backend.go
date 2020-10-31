147a372341f923ef263ea5c95ce8b5321c2be14b
salmin36 <salmin36@gmail.com>
2020-10-31 21:22:24 +0200

    Added redis connection from backend

--- backend/backend.go
+++ backend/backend.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
+
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	fmt.Println("Hello World")
+
+	go connectToRedis()
+
 	for {
 
 	}
 	wg.Wait()
+}
+
+var ctx = context.Background()
+
+func connectToRedis() {
+	db := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "temp_pass_123", // no password set
+		DB:       0,               // use default DB
+	})
+
+	err := db.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := db.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := db.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/salmin36/favorite_foods/backend
 
 go 1.15
+
+require github.com/go-redis/redis/v8 v8.3.3
