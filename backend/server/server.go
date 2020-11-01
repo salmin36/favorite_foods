fd50b85e6a942add714004319747b2eaec4af5df
salmin36 <salmin36@gmail.com>
2020-11-01 15:10:39 +0200

    Removed unnecesary middleware

--- backend/server/server.go
+++ backend/server/server.go
@@ -31,18 +31,8 @@ func NewServer() *Server {
 
 func (server *Server) StartServer() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.Use(server.commondMiddleware)
 	router.HandleFunc("/foods", server.FoodsEndpoint).Methods("GET")
 	http.ListenAndServe(":8081", router)
-}
-
-func (server *Server) commondMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
-		next.ServeHTTP(w, r)
-	})
 }
 
 func (server *Server) FoodsEndpoint(w http.ResponseWriter, r *http.Request) {
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -4,21 +4,6 @@ const app = express()
 const port = 8080
 const request = require("request");
 
-/*
-var whitelist = ['http://backend', "https://backend", "https://frontend", "http://127.0.0.1", "https://127.0.0.1"]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-res.
-app.use(cors(corsOptions))
-*/
 
 app.use('/', express.static('./dist', {
   index: "index.html"
@@ -42,35 +27,5 @@ app.get('/foods', function(req, res) {
 
 
 
-/*request('http://backend:8081/foods',function(error, response, body){
-    if(error.body != null) {
-      console.error("Error happened")
-      res.json("{'Error':'Could not find endpoint'}")
-    } else {
-      res.send(response)
-    }  
-    
-  })
-  */ 
-/*
-
-app.get('/foods', async (req, res) => {
-  const uri = 'http://backend:8081/foods'
-  const resp =  await reque(uri).catch(r => {
-    
-    res.json("{'error':'service not found'}")
-  })
-  res.json(resp)
-  
-
-})
-*/
-
-/*
-app.use('/foods', createProxyMiddleware({
-  target: 'http://backend:8081/foods',
-  
-  }));
-*/
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
